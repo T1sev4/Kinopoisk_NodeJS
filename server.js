9c7a8de709354e31d95b655c1ebd20707afabea4
t1sev4 <tamikofficial@gmail.com>
2023-04-03 11:07:12 +0600

    Start project kinopoisk

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+app.get('/admin', (req, res) => {
+  res.render('adminProfile');
+});
+app.get('/new', (req, res) => {
+  res.render('newFilm');
+});
+app.get('/edit', (req, res) => {
+  res.render('editFilm');
+});
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
