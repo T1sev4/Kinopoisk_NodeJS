92e3b2243170cf517b1d28d3cc10d3921f30f4e6
t1sev4 <tamikofficial@gmail.com>
2023-04-11 23:39:29 +0600

    add register

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ require('./server/config/db');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded());
 // подключение файла в котором находится рендер страниц
 app.use(require('./server/pages/router'));
 
 app.use(require('./server/Genres/router'));
 app.use(require('./server/Country/router'));
-
+app.use(require('./server/auth/router'));
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
---
+++ server/auth/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  email: String,
+  full_name: String,
+  password: String,
+});
+
+module.exports = mongoose.model('user', UserSchema);
