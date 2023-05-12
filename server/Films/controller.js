b2d70f2409970138c8711d885f401a5f393f1564
t1sev4 <tamikofficial@gmail.com>
2023-05-12 14:44:42 +0600

    delete toWatch films from index page and profile page and create film detail page

--- server/Films/controller.js
+++ server/Films/controller.js
@@ -2,6 +2,7 @@ const Film = require('./Film');
 const User = require('../auth/User');
 const fs = require('fs');
 const path = require('path');
+const { log } = require('console');
 
 const createFilm = async (req, res) => {
   if (
@@ -93,6 +94,20 @@ const saveFilm = async (req, res) => {
       res.send('Фильм уже сохранен')
     }
   }
- 
 }
-module.exports = { createFilm, editFilm, deleteFilm, saveFilm };
+
+const deleteFromToWatch = async (req, res) => {
+  if(req.user && req.params.id){
+    const user = await User.findById(req.user.id)
+    for (let i = 0; i < user.toWatch.length; i++) {
+      if(user.toWatch[i] == req.params.id){
+        user.toWatch.splice(i, 1);
+        user.save()
+        res.send('Успешно удалено');
+      }
+    }
+    // res.send('Данные не найдены');
+  }
+}
+
+module.exports = { createFilm, editFilm, deleteFilm, saveFilm, deleteFromToWatch };
--- server/pages/router.js
+++ server/pages/router.js
@@ -65,5 +65,9 @@ router.get('/edit/:id', async (req, res) => {
 });
 router.get('/not-found', (req, res) => {
   res.render('notFound');
-});
+}); 
+
+router.get('/detail/:id', (req, res) => {
+  res.render('detail', {user: {}}); 
+})
 module.exports = router;
