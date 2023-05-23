3e38bb9b196ec7d820be9844b898c3e6aa1a510a
t1sev4 <tamikofficial@gmail.com>
2023-05-24 02:17:23 +0600

    add comments

--- server/pages/router.js
+++ server/pages/router.js
@@ -4,7 +4,7 @@ const Genres = require('../Genres/Genres');
 const Country = require('../Country/Country');
 const User = require('../auth/User');
 const Film = require('../Films/Film')
-
+const Rate = require('../Rates/Rates');
 router.get('/', async (req, res) => {
   // фильтрация по категориям //
   const options = {};
@@ -101,7 +101,12 @@ router.get('/not-found', (req, res) => {
 }); 
 
 router.get('/detail/:id', async (req, res) => {
+  const rates = await Rate.find({filmId: req.params.id}).populate('authorId');
+  let averageRate = 0;
+  for (let i = 0; i < rates.length; i++) {
+    averageRate += rates[i].rate    
+  }
   const film = await Film.findById(req.params.id).populate('country').populate('genre')
-  res.render('detail', {user: req.user ? req.user : {}, film}); 
+  res.render('detail', {user: req.user ? req.user : {}, film, rates, averageRate: (averageRate / rates.length).toFixed(1)}); 
 })
 module.exports = router;
